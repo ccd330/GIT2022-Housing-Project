4b7c76e6bd32291d2f306b6f3f526b8d3ba9193e
ccd330 <claudiacdavis@gmail.com>
2022-10-01 18:57:44 -0400

    partnerships feature need to fix background coloring

--- components/About/index.js
+++ components/About/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import beigebuilding from '../../assets/right-image-buildingA.png'
 import yellowchair from '../../assets/yellow-chair.png'
 import bluehouse from '../../assets/blue-house.png'
+import ryan from '../../assets/logo-ryan.png'
+import brodsky from '../../assets/logo-brodsky.png'
+import hickman from '../../assets/logo-HRgroup.png'
+import green from '../../assets/logo-GP.png'
 
 function About () {
     return (
@@ -20,12 +24,23 @@ function About () {
             </div>
         </div>
         <div className="mainBody">
-            <div className="row">
+            <div className="boxes">
+                <div className="row">
                 <img src={yellowchair} alt="yellow-chair" className='yellow-chair' />
                 <img src={bluehouse} alt="blue-house" className='blue-house' />
             </div>
+            </div>
+            <div className="partnerships">
+                <h1>Partnerships</h1>
+                <p>Lorem ipsum.</p>
+                <div className='row'>
+                    <img src={ryan} alt="ryan-king-team-logo"/>
+                    <img src={brodsky} alt="brodsky-logo"/>
+                    <img src={hickman} alt="hickman-logo"/>
+                    <img src={green} alt="hickman-logo"/>
+                </div>
+            </div>
                 <h2>Testimonial</h2>
-                <h2>Partnerships</h2>
                 <h2>Refer A Friend</h2>
                 <h2>Q&A</h2>
             </div>
